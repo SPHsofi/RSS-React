65d016e9c181cf0d0d92891efbd99c7a496294ad
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-07-08 23:07:24 +0300

    feat: add api connection to swager animal api

---
+++ class-components/src/API/api.tsx
@@ -0,0 +1,48 @@
+const API_URL: string = 'https://stapi.co/api/v1/rest/animal/search';
+
+export interface Animal {
+  uid: string;
+  name: string;
+  earthAnimal: boolean;
+  earthInsect: boolean;
+  avian: boolean;
+  canine: boolean;
+  feline: boolean;
+}
+
+interface Page {
+  firstPage: boolean;
+  lastPage: boolean;
+  numberOfElements: number;
+  pageNumber: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+interface Sort {
+  clauses: [];
+}
+
+interface ServerResponse {
+  page: Page;
+  sort: Sort;
+  animals: Animal[];
+}
+
+const animal = async (): Promise<Animal[]> => {
+  try {
+    const response = await fetch(`${API_URL}`);
+
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    const data: ServerResponse = await response.json();
+    return data.animals;
+  } catch (error) {
+    console.error('API error:', error);
+  }
+};
+
+export default animal;
