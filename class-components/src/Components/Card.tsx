08bb0162c02f4f067abe7da8d896b17c2d926574
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-07-09 16:30:50 +0300

    refactor: change type in interface, change render card in condition render one or more card

--- class-components/src/Components/Card.tsx
+++ class-components/src/Components/Card.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import { Animal } from '../API/api.tsx';
-import style from '../Styles/Card.module.css'
+import style from '../Styles/Card.module.css';
 
 interface CardProps {
-  animals: Animal[];
+  animals: Animal[] | string;
 }
 
 interface CardState {}
 
 class Card extends React.Component<CardProps, CardState> {
-  render() {
+  renderAnimal = (animal: Animal) => {
+    const faunaList = [];
+    if (animal.earthAnimal) faunaList.push('Earth Animal');
+    if (animal.earthInsect) faunaList.push('Earth Insect');
+    if (animal.feline) faunaList.push('Feline');
+    if (animal.avian) faunaList.push('Avian');
+    if (animal.canine) faunaList.push('Canine');
+
+    const faunaString = faunaList.join(', ');
+
     return (
-      <div className={style.wrapper}>
-        {this.props.animals.map((animal, ind) => {
-          const faunaList = [];
-          if (animal.earthAnimal) faunaList.push('Earth Animal');
-          if (animal.earthInsect) faunaList.push('Earth Insect');
-          if (animal.feline) faunaList.push('Feline');
-          if (animal.avian) faunaList.push('Avian');
-          if (animal.canine) faunaList.push('Canine');
-
-          const faunaString = faunaList.join(', ');
-
-          return (
-            <div key={ind} className={style.cardWrap}>
-              <p>Uid: {animal.uid}</p>
-              <p>Name: {animal.name}</p>
-              <p>Fauna: {faunaString ? faunaString : 'No fauna listed'}</p>
-            </div>
-          );
-        })}
+      <div key={animal.uid} className={style.cardWrap}>
+        <p>Uid: {animal.uid}</p>
+        <p>Name: {animal.name}</p>
+        <p>Fauna: {faunaString ? faunaString : 'No fauna listed'}</p>
       </div>
     );
+  };
+
+  render() {
+    const { animals }: string = this.props;
+
+    if (Array.isArray(animals)) {
+      return <div className={style.wrapper}>{animals.map(this.renderAnimal)}</div>;
+    } else {
+      return <div className={style.wrapper}>{this.renderAnimal(animals)}</div>;
+    }
   }
 }
 
