afd39e5c2a17c47db87cad1f2d412247a290ffb8
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-07-09 16:27:32 +0300

    feat: add connect to api to find single animal

---
+++ class-components/src/API/FindApi.tsx
@@ -0,0 +1,24 @@
+import { Animal } from './api.tsx';
+
+export interface Resp {
+  animal: Animal;
+}
+
+const API_URL: string = 'https://stapi.co/api/v1/rest/animal';
+
+const findApi = async (uid: string): Promise<Resp> => {
+  try {
+    const response = await fetch(`${API_URL}?uid=${uid}`);
+
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    const data: Resp = await response.json();
+    return data;
+  } catch (error) {
+    console.error('API error:', error);
+  }
+};
+
+export default findApi;
